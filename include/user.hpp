447b62c508fa04f47a75747f9c95452b360d7c21
Athmane Naciri <anaciri@e1r6p10.1337.ma>
2023-11-01 21:45:48 +0100

    add new class for user

---
+++ include/user.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include<unistd.h>
+#include<string>
+#include<stdio.h>
+#include<cstring>
+#include<stdlib.h>
+#include<sys/types.h>
+#include <netinet/in.h>
+#include<sys/socket.h>
+#include <arpa/inet.h>
+#include<fcntl.h>
+
+class user {
+	public:
+		user();
+		~user();
+};
